57cecfe952ef465c688dcc9bb077b666f562582f
Kirill Sermyagin <sermyagin.abcen7@gmail.com>
2025-03-02 05:34:08 +0300

    Basket beauty

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -35,15 +35,38 @@ export default function Cart() {
 
   return (
     <Container>
-      <Button onClick={() => router.push('/pay')}>Оформить заказ</Button>
-      <ProductSection
-        products={cartProducts.map((product) => ({
-          ...product.product,
-          image: product.product.images?.[0]?.url
-        }))}
-        title="Корзина"
-        type="cart"
-      />
+      <div className="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-gray-900">Корзина</h1>
+        <div className="space-y-8">
+          <ProductSection
+            products={cartProducts.map((product) => ({
+              ...product.product,
+              image: product.product.images?.[0]?.url
+            }))}
+            type="cart"
+            title=""
+          />
+          <div className="flex justify-between items-center border-t">
+            <div className="text-lg font-medium">
+              Итого: ₽
+              {cartProducts
+                .reduce((total, item) => {
+                  const price = item.product.price
+                  const discount = item.product.discount || 0
+                  const finalPrice = discount ? price * (1 - discount / 100) : price
+                  return total + finalPrice * item.quantity
+                }, 0)
+                .toFixed(2)}
+            </div>
+            <Button
+              onClick={() => router.push('/pay')}
+              className="text-lg font-semibold bg-primary hover:bg-primary/90 transition-colors"
+            >
+              Оформить заказ
+            </Button>
+          </div>
+        </div>
+      </div>
     </Container>
   )
 }
