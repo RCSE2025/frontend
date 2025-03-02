e2cb1785387ef123255c4a519754ebbe65e0bef6
atrepalin <atrepalin19@gmail.com>
2025-03-02 05:21:23 +0300

    Add full cart logic

--- app/@modal/(.)pay/page.tsx
+++ app/@modal/(.)pay/page.tsx
@@ -4,17 +4,60 @@ import { PageModal } from '@/components/shared/modal'
 import { Title } from '@/components/shared/title'
 import { Ton } from '@/components/shared/ton'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { TON_PRICE } from '@/config'
+import { createManualOrder, createYookassaOrder } from '@/shared/api/payments/methods'
+import { useCart } from '@/shared/store/useCart'
+import { useRouter } from 'next/navigation'
+import React from 'react'
 
 export default function Pay() {
-  const amount = 1000
+  const [address, setAddress] = React.useState('')
+
+  const { products, prices } = useCart()
+
+  const [amount, setAmount] = React.useState(0)
+
+  React.useEffect(() => {
+    setAmount(
+      Object.entries(products).reduce((acc, [id, quantity]) => acc + quantity * prices[id], 0)
+    )
+  })
+
+  const router = useRouter()
 
   return (
     <PageModal>
       <div className="flex flex-col gap-4">
         <Title>Оплата ₽{amount}</Title>
-        <Button className="bg-theme w-full">UKassa</Button>
-        <Ton amount={amount / TON_PRICE} onSend={console.log} className="w-[200px]" />
+        <Title>Введите адрес отправления</Title>
+        <Input
+          className="w-full"
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <Button
+          className="bg-theme w-full"
+          onClick={async () => {
+            const url = await createYookassaOrder(address)
+
+            window.open(url, '_blank')
+
+            window.location.href = '/'
+          }}
+        >
+          YooKassa
+        </Button>
+        <Ton
+          amount={amount / TON_PRICE}
+          onSend={async () => {
+            await createManualOrder(address)
+
+            window.location.href = '/'
+          }}
+          className="w-full"
+        />
       </div>
     </PageModal>
   )
--- shared/store/useCart.ts
+++ shared/store/useCart.ts
@@ -4,13 +4,19 @@ interface ICartStore {
   products: {
     [key: string | number]: number
   }
+  prices: {
+    [key: string | number]: number
+  }
   updateProductQuantity: (id: string | number, quantity: number) => void
   setProducts: (products: { [key: string | number]: number }) => void
+  setPrices: (prices: { [key: string | number]: number }) => void
 }
 
 export const useCart = create<ICartStore>((set) => ({
   products: {},
+  prices: {},
   updateProductQuantity: (id: string | number, quantity: number) =>
     set((state) => ({ products: { ...state.products, [id]: quantity } })),
-  setProducts: (products: { [key: string | number]: number }) => set({ products })
+  setProducts: (products: { [key: string | number]: number }) => set({ products }),
+  setPrices: (prices: { [key: string | number]: number }) => set({ prices })
 }))
